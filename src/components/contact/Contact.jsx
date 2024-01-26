0ada773c668f5bc6041f7bf21d8fd3a32952517e
Pedro Fede Casillas <pedrofedecasillas2@gmail.com>
2024-01-26 14:57:53 -0300

    fix alert contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,14 @@
 import axios from "axios";
 import styles from "../contact/contact.module.css"
-import {MdDeleteForever} from "react-icons/md"
-import {FaHeart} from "react-icons/fa"
+import {MdDeleteForever} from "react-icons/md";
+import {FaHeart} from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const API_URL = "http://localhost:3001"
 
 export default function Contact({ contact, setContacts }) {
   const { id, name, address, phone, email, favorite } = contact;
-
+  
   const handleFavorite = async (event) => {
     try {
       const updatedContact = {
@@ -26,18 +27,72 @@ export default function Contact({ contact, setContacts }) {
     }
   };
 
+  // const handleUpdate = async (event) => {
+  //   try {
+  //     const updatedContact = {
+  //       name,
+  //       address,
+  //       phone,
+  //       email,
+  //       favorite,
+  //     };
+  
+  //     const { data } = await axios.put(`${API_URL}/contact/${id}`, updatedContact);
+  //     await setContacts(data);
+  
+  //     console.log('Contact updated successfully.');
+  //   } catch (error) {
+  //     console.error('Error updating contact:', error.message || error);
+  //   }
+  // }
+  
+
   const handleDelete = async (event) => {
-      try {
-        const { data } = await axios.delete(
-            `${API_URL}/contact/${id}`
-          );
-          await setContacts(data);
-          alert('Contacto eliminado con éxito');
+    try {
+      const result = await Swal.fire({
+        title: "Estas seguro?",
+        text: "¡No podrás revertir esto!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si eliminar!"
+      });
+  
+      if (result.isConfirmed) {
+        const { data } = await axios.delete(`${API_URL}/contact/${id}`);
+        await setContacts(data);
+  
+        await Swal.fire({
+          title: "¡Eliminado!",
+          text: "Se eliminó con éxito.",
+          icon: "success"
+        });
+      }
     } catch (error) {
-        console.log(error);
-        alert('No se pudo eliminar el contacto. Por favor, inténtelo de nuevo.');
+      console.log(error);
+  
+      await Swal.fire({
+        title: "Error",
+        text: "No se puede eliminar el contacto. Inténtalo de nuevo.",
+        icon: "error"
+      });
     }
   }
+  
+
+  // const handleDelete = async (event) => {
+  //     try {
+  //       const { data } = await axios.delete(
+  //           `${API_URL}/contact/${id}`
+  //         );
+  //         await setContacts(data);
+  //         alert('Contacto eliminado con éxito');
+  //   } catch (error) {
+  //       console.log(error);
+  //       alert('No se pudo eliminar el contacto. Por favor, inténtelo de nuevo.');
+  //   }
+  // }
 
   return (
     <div className={styles.contactContainer}>
@@ -58,6 +113,8 @@ export default function Contact({ contact, setContacts }) {
       <button className={styles.btndelete} onClick={handleDelete}>
         <MdDeleteForever className={styles.btndeleteIcon}/>
         </button>
+
+        {/* <button onClick={handleUpdate}>editar</button> */}
       
     </div>
   );
